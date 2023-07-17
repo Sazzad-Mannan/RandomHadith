866f5ec608468958461da662153de8208bad268a
Sazzad-Mannan <sazzadmannan16@gmail.com>
2023-07-17 23:41:55 +0600

    commit

--- app/src/main/java/com/riftech/randomhadith/MainActivity.java
+++ app/src/main/java/com/riftech/randomhadith/MainActivity.java
@@ -1,6 +1,7 @@
 package com.riftech.randomhadith;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -20,11 +21,22 @@ import com.google.android.gms.ads.initialization.OnInitializationCompleteListene
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;*/
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.ump.ConsentDebugSettings;
+import com.google.android.ump.ConsentForm;
+import com.google.android.ump.ConsentInformation;
+import com.google.android.ump.ConsentRequestParameters;
+import com.google.android.ump.FormError;
+import com.google.android.ump.UserMessagingPlatform;
 
 import java.util.Random;
 
@@ -32,16 +44,20 @@ import java.util.Random;
 
 public class MainActivity extends hadith {
     private TextView et1;
-    private Integer code=1;
+    private Integer code = 1;
     //private InterstitialAd mInterstitialAd;
-    private String jsonString="";
+    private String jsonString = "";
     private static final String TAG = "MainActivity";
-    private String percentage=null;
+    private String percentage = null;
     String hadis;
     private ProgressBar pgsBar;
     private View v;
+    int count=0;
     String arraynumber;
+    private InterstitialAd mInterstitialAd;
     private AdView mAdView;
+    private ConsentInformation consentInformation;
+    private ConsentForm consentForm;
 
 
     @Override
@@ -49,47 +65,90 @@ public class MainActivity extends hadith {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//                ConsentDebugSettings debugSettings = new ConsentDebugSettings.Builder(this)
+//                .setDebugGeography(ConsentDebugSettings.DebugGeography.DEBUG_GEOGRAPHY_EEA)
+//                .addTestDeviceHashedId("C0C3585D7681D1732E0C5A43C804A1C6")
+//                .build();
+//
+//
+//        ConsentRequestParameters params = new ConsentRequestParameters
+//                .Builder()
+//                .setConsentDebugSettings(debugSettings)
+//                .build();
+
+
+
+        // Set tag for under age of consent. false means users are not under
+        // age.
+        ConsentRequestParameters params = new ConsentRequestParameters
+                .Builder()
+                .setTagForUnderAgeOfConsent(false)
+                .build();
+
+        consentInformation = UserMessagingPlatform.getConsentInformation(this);
+        consentInformation.requestConsentInfoUpdate(
+                this,
+                params,
+                new ConsentInformation.OnConsentInfoUpdateSuccessListener() {
+                    @Override
+                    public void onConsentInfoUpdateSuccess() {
+                        // The consent information state was updated.
+                        // You are now ready to check if a form is available.
+
+                        if (consentInformation.isConsentFormAvailable()) {
+
+                            loadForm();
+                        }
+                    }
+                },
+                new ConsentInformation.OnConsentInfoUpdateFailureListener() {
+                    @Override
+                    public void onConsentInfoUpdateFailure(FormError formError) {
+                        // Handle the error.
+                    }
+                });
+
 
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
             }
         });
-        //loadAd();
+        loadAd();
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
 
         pgsBar = (ProgressBar) findViewById(R.id.pBar);
-        et1=(TextView) findViewById(R.id.textView);
-
+        et1 = (TextView) findViewById(R.id.textView);
 
 
     }
-    public void start_match(final View view){
+
+    public void start_match(final View view) {
         pgsBar.setVisibility(view.VISIBLE);
-        final String name1=et1.getText().toString();
-        arraynumber= Integer.toString(hadith.length) ;
-        hadis=getRandom(hadith);
+        final String name1 = et1.getText().toString();
+        arraynumber = Integer.toString(hadith.length);
+        hadis = getRandom(hadith);
         pgsBar.setVisibility(view.GONE);
-        //showInterstitial();
-        et1.setText (hadis);
+        count++;
+        showInterstitial();
 
 
     }
 
 
-    public static String getRandom(String [] array) {
+    public static String getRandom(String[] array) {
         int rnd = new Random().nextInt(array.length);
         return array[rnd];
     }
 
-    /*public void loadAd() {
+    public void loadAd() {
         AdRequest adRequest2 = new AdRequest.Builder().build();
-//ca-app-pub-7831928589958637/9194456092
+//ca-app-pub-7831928589958637/8842441536
 //ca-app-pub-3940256099942544/1033173712
-        InterstitialAd.load(this,"ca-app-pub-7831928589958637/9194456092", adRequest2,
+        InterstitialAd.load(this, "ca-app-pub-7831928589958637/8842441536", adRequest2,
                 new InterstitialAdLoadCallback() {
                     @Override
                     public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
@@ -109,7 +168,9 @@ public class MainActivity extends hadith {
                                         Log.d("TAG", "The ad was dismissed.");
 
                                         loadAd();
-                                        et1.setText (joke);
+count=0;
+                                        //showInterstitial();
+                                        et1.setText(hadis);
                                         //gotoActivity(intent);
                                         //dismissed();
 
@@ -143,7 +204,7 @@ public class MainActivity extends hadith {
                                         loadAdError.getDomain(), loadAdError.getCode(), loadAdError.getMessage());
                         Toast.makeText(
                                 MainActivity.this, "onAdFailedToLoad() with error: " + error, Toast.LENGTH_SHORT)
-                                .show();
+                                .show();*/
                     }
 
                 });
@@ -151,21 +212,53 @@ public class MainActivity extends hadith {
 
     private void showInterstitial() {
         // Show the ad if it's ready. Otherwise toast and restart the game.
-        if (mInterstitialAd != null) {
+        if (mInterstitialAd != null && count>2) {
             mInterstitialAd.show(this);
 
 
-        }
-
-        else {
+        } else {
             //Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
             loadAd();
-            et1.setText (joke);
+            et1.setText(hadis);
             //gotoActivity(intent);
             //startGame();
-        }*/
+        }
     }
 
+    public void loadForm() {
+        // Loads a consent form. Must be called on the main thread.
+        UserMessagingPlatform.loadConsentForm(
+                this,
+                new UserMessagingPlatform.OnConsentFormLoadSuccessListener() {
+                    @Override
+                    public void onConsentFormLoadSuccess(ConsentForm consentForm) {
+                        MainActivity.this.consentForm = consentForm;
+                        if (consentInformation.getConsentStatus() == ConsentInformation.ConsentStatus.REQUIRED) {
+                            consentForm.show(
+                                    MainActivity.this,
+                                    new ConsentForm.OnConsentFormDismissedListener() {
+                                        @Override
+                                        public void onConsentFormDismissed(@Nullable FormError formError) {
+                                            if (consentInformation.getConsentStatus() == ConsentInformation.ConsentStatus.OBTAINED) {
+                                                // App can start requesting ads.
+                                            }
 
+                                            // Handle dismissal by reloading form.
+                                            loadForm();
+                                        }
+                                    });
+                        }
+                    }
+                },
+                new UserMessagingPlatform.OnConsentFormLoadFailureListener() {
+                    @Override
+                    public void onConsentFormLoadFailure(FormError formError) {
+                        // Handle the error.
+                    }
+                }
+        );
+    }
+
+}
 
 
